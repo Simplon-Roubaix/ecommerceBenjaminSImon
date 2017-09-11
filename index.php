80dcc7283ac5d06e7737864f3c111e8977f6c29b
benjamin <degroote.benjamin@gmail.com>
2017-09-11 10:00:35 +0200

    add link for boostrap in header and script in footer

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>E-Commerce</title>
-    <link src="css/main.css"
-  </head>
-  <body>
-    <header>
         <?php include("header.php"); ?>
-    </header>
-    <footer>
+
         <?php include("footer.php"); ?>
-    </footer>
-  </body>
-</html>
+  

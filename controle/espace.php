cf869efeab9a06ece0c6c23bb46c894e984e80c5
benjamin <degroote.benjamin@gmail.com>
2017-09-22 11:41:12 +0200

    insert into img and article finish in espace.php

--- controle/espace.php
+++ controle/espace.php
@@ -8,8 +8,21 @@ if(isset($_GET['deleteid'])){
 delete_article($_GET['deleteid']);
 }
 if(isset($_POST['titre']) and isset($_POST['description']) and isset($_POST['descriptiondetail']) and isset($_POST['descriptionimg']) and isset($_FILES['file'])){
-  add_article($_POST['titre'],$_POST['description'],$_POST['descriptiondetail']);
-  add_image($_FILES['file'],$_POST['descriptionimg']);
+  if (isset($_FILES['file']) and $_FILES['file']['error'] == 0 and !empty($_POST['titre']) && !empty($_POST['description'])) {
+      if ($_FILES['file']['size'] <= 1000000) {
+          $uploads_dir = '../img/';
+          $nomfichier =$_FILES['file']['name'];
+          $infosfichier = pathinfo($_FILES['file']['name']);
+          $extension_upload = $infosfichier['extension'];
+          $tmp_name = $_FILES["file"]["tmp_name"];
+          $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+          if (in_array($extension_upload, $extensions_autorisees)) {
+              move_uploaded_file($tmp_name,"$uploads_dir/$nomfichier");
+              add_image($nomfichier,$_FILES['file']['size'],$extension_upload,$_POST['descriptionimg']);
+              add_article($_POST['titre'],$_POST['description'],$_POST['descriptiondetail']);
+}
+}
+};
 }
 if(isset($_POST['idupdate'])){
 update_article($_POST['titre'], $_POST['description'], $_POST['descriptiondetail'], $_POST['idupdate']);
